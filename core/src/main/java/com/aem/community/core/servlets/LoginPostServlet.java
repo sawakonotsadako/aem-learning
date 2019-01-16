5a1dc3779f79d39435fef5724165c46ab78b5cb0
vincent.yang <vincent.yang@ncs.com.sg>
2019-01-16 17:48:26 +0800

    first commit

---
+++ core/src/main/java/com/aem/community/core/servlets/LoginPostServlet.java
@@ -0,0 +1,8 @@
+package com.aem.community.core.servlets;
+
+import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+
+public class LoginPostServlet extends SlingAllMethodsServlet {
+}
